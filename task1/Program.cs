bcb7d50491f56b903c3ca83e8ae61a98ff9a7865
timphp <jttj11@gmail.com>
2017-06-29 22:37:00 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 0, 6, 9, 42, 34, 26, 47, 62, 2, 1, 54, 36, 999, 3, 5 };
+            foreach (int el in arr) Console.Write(el + " ");
+            Console.Write("\n");
+            Sort(ref arr);
+            foreach (int el in arr) Console.Write(el + " ");
+            Console.Write("\n");
+        }
+
+        private static void Sort(ref int[] arr, int left = 0, int right = -1)
+        {
+            if (right == -1) right = arr.Length - 1;
+            if (left < right)
+            {
+                int p = Partition(ref arr, left, right);
+                Sort(ref arr, left, p);
+                Sort(ref arr, p + 1, right);
+            }
+        }
+
+        static int Partition(ref int[] arr, int left, int right)
+        {
+            int pivot = Mediana(arr[left], arr[(right - left) / 2], arr[right]);
+            while (left < right)
+            {
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+                if (left >= right)
+                {
+                    return right;
+                }
+                arr[left] += arr[right];
+                arr[right] = arr[left] - arr[right];
+                arr[left] -= arr[right];
+            }
+            return right;
+        }
+
+        static int Mediana(int a, int b, int c)
+        {
+            return a > b & b > c || a < b & b < c ? b :
+                   a > c & c > b || a < c & c < b ? c : a;
+        }
+    }
+}
